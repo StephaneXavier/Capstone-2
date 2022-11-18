c93245b4067c732f2703af658ab5346b810c9667
stephane <stephanexc@gmail.com>
2022-11-18 16:04:44 -0500

    changes to deploy to heroku

--- db.js
+++ db.js
@@ -1,7 +1,7 @@
 const { Client } = require("pg");
-const { DB_URI } = require("./config");
 
-const client = new Client(DB_URI);
+
+const client = new Client(process.env.DATABASE_URL);
 
 client.connect();
 
