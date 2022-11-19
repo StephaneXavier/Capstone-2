2da94af197df26b742d5564d7492f1054ab32ead
stephane <stephanexc@gmail.com>
2022-11-19 14:00:36 -0500

    change db ssl config to deploy to heroku

--- db.js
+++ db.js
@@ -1,7 +1,12 @@
 const { Client } = require("pg");
-const {DB_URI} = require('./config')
+const { DB_URI } = require('./config')
 
-const client = new Client({ connectionString: DB_URI });
+const client = new Client({
+    connectionString: DB_URI,
+    ssl: {
+        rejectUnauthorized: false
+    }
+});
 
 client.connect();
 
